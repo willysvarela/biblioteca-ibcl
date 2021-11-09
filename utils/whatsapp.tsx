5b26e0eccd68c35326aa99e8eefa4844b08a938a
willysv <willysv@motorola.com>
2021-11-08 20:38:36 -0400

    fix: function parameters more than one

--- utils/whatsapp.tsx
+++ utils/whatsapp.tsx
@@ -1,6 +1,6 @@
 import { Book } from "../interfaces/interfaces";
 
-export const createWhatsappLink = (book: Book, url: string) => { 
+export const createWhatsappLink = (book: Book) => { 
 	const message = `${process.env.NEXT_PUBLIC_WHATSAPP_MESSAGE}%0a ${book.title} - ID: ${book.id}%0a${window.location}`;
 	const link = `${process.env.NEXT_PUBLIC_WHATSAPP_URL}${process.env.NEXT_PUBLIC_PHONE_NUMBER}?text=${message}`;
 	return link;
